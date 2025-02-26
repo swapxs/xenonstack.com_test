54d234bc8ee0cb580eae1dec618946b4c10509b5
swapxs <swapnil.bhowmik@xenonstack.com>
2025-02-26 13:40:15 +0530

    Test 3
    This test detects if there is any SQL injection vulneribity in the form
    if found will report back that the site is vulnerabile, if not it will print
    success.
    
    Also I have formated the code for better reability as recommended by my linter

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ OPT.add_argument("--start-maximized")
 OPT.add_argument("--disable-blink-features=AutomationControlled")
 OPT.add_argument("--headless=new")  # Run headless
 
-SERVICE = Service("/usr/bin/chromedriver", log_output=None)
+SERVICE = Service("/usr/bin/chromedriver", log_output="/dev/null")
 DRIVER = webdriver.Chrome(service=SERVICE, options=OPT)
 DRIVER.get("https://www.xenonstack.com/")
 
@@ -31,9 +31,10 @@ def test_required_field():
         err = DRIVER.find_elements(By.CLASS_NAME, "error-message")
         assert len(err) > 0, "Error message did not appear"
 
-        printc("[bold green][ SUCCESS ][/bold green] Required Filed Validation Test Passed")
+        printc("[bold green][ SUCCESS ][/bold green]"
+               "Required Filed Validation Test Passed")
 
-    except Exception as e:
+    except Exception:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         printc("[bold red][ ERROR ][/bold red]", exc_type, fname, exc_tb.tb_lineno)
@@ -49,14 +50,44 @@ def test_invalid_inputs():
         dropdown = Select(DRIVER.find_element(By.ID, "enterpriseIndustry"))
         dropdown.select_by_index(2)
 
-        DRIVER.find_element(By.XPATH, "//p[normalize-space()='Proceed Next']").click()
+        DRIVER.find_element(By.XPATH,
+                            "//p[normalize-space()='Proceed Next']"
+                            ).click()
 
         err = DRIVER.find_elements(By.CLASS_NAME, "error-message")
         assert len(err) > 0, "Invalid inputs were accepted!"
 
-        printc("[bold green][ SUCCESS ][/bold green] Invalid Input Handling Test Passed")
+        printc("[bold green][ SUCCESS ][/bold green]"
+               " Invalid Input Handling Test Passed")
 
-    except Exception as e:
+    except Exception:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        printc("[bold red][ ERROR ][/bold red]", exc_type, fname, exc_tb.tb_lineno)
+
+
+def test_injection_SQL():
+    try:
+        payload = "' OR '1'='1'; --"
+
+        DRIVER.find_element(By.NAME, "firstname").send_keys(payload)
+        DRIVER.find_element(By.NAME, "lastname").send_keys(payload)
+        DRIVER.find_element(By.NAME, "email").send_keys("test.site@corp.com")
+        DRIVER.find_element(By.NAME, "contact").send_keys("abcd1234")
+        DRIVER.find_element(By.NAME, "company").send_keys("testsite")
+        dropdown = Select(DRIVER.find_element(By.ID, "enterpriseIndustry"))
+        dropdown.select_by_index(2)
+
+        DRIVER.find_element(By.XPATH,
+                            "//p[normalize-space()='Proceed Next']"
+                            ).click()
+
+        assert "error" in DRIVER.page_source.lower(), "Vulnerability Detected"
+
+        printc("[bold green][ SUCCESS ][/bold green]"
+               " SQL Injection Detection Test Passed")
+
+    except Exception:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         printc("[bold red][ ERROR ][/bold red]", exc_type, fname, exc_tb.tb_lineno)
@@ -65,4 +96,5 @@ def test_invalid_inputs():
 if __name__ == "__main__":
     test_required_field()
     test_invalid_inputs()
+    test_injection_SQL()
     DRIVER.quit()
