496714457cac0652b0e454001cf5407a8e26f71d
swapxs <swapnil.bhowmik@xenonstack.com>
2025-03-03 08:46:50 +0530

    moved security to another file :

--- functional/tests/test_form.py
+++ functional/tests/test_form.py
@@ -6,10 +6,8 @@ from utils.rich_config import printc
 from utils.selenium_config import DRIVER
 from helpers.page_loader import page_loader
 
-from helpers.form_handler import (
-    injkt,
-    fill_xenon_form
-)
+from helpers.form_handler import fill_xenon_form
+
 
 # This function tests if the fileds in the form are "required" by nature and
 # checks if we can proceed and see to content of the next page without filling
@@ -43,7 +41,6 @@ def test_required_field():
                    " Required Filed Validation Test",
                    "[success]Passed[/success]."
                    " It does not let us go through without entering data.")
-
 
     except Exception:
         exc_type, _, exc_tb = sys.exc_info()
@@ -150,72 +147,6 @@ def test_valid_inputs():
                    "[success]Passed[/success].",
                    "Does not let us go through without providing proper",
                    "inputs.")
-
-    except Exception:
-        exc_type, _, exc_tb = sys.exc_info()
-        if exc_tb is not None:
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            printc("[bug][ x ][/bug]",
-                   exc_type, fname, exc_tb.tb_lineno)
-
-
-def test_injection_SQL():
-    printc("\n[head]Test 1.4: [/head]"
-           " Testing For SQL Injection.")
-    try:
-        DRIVER.get("https://xenonstack.com/")
-        page_loader()
-
-        printc("[bold yellow][ * ][/bold yellow] Get Started Button Clicked")
-        DRIVER.find_element(By.CLASS_NAME, "nav-button").click()
-        time.sleep(1)
-
-        injkt("' OR '1'='1'; --")
-
-        printc("[bold yellow][ * ][/bold yellow] Proceed Next Button Clicked")
-        DRIVER.find_element(By.XPATH,
-                            "//p[normalize-space()='Proceed Next']"
-                            ).click()
-
-        if "error" not in DRIVER.page_source.lower():
-            printc("[bug][ x ][/bug] Vulnerability Detected")
-
-        else:
-            printc("[success][ + ][/success]"
-                   " SQL Injection Detection Test Passed")
-
-    except Exception:
-        exc_type, _, exc_tb = sys.exc_info()
-        if exc_tb is not None:
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            printc("[bug][ x ][/bug]",
-                   exc_type, fname, exc_tb.tb_lineno)
-
-
-def test_injection_XSS():
-    printc("\n[head]Test 1.5: [/head]"
-           " Testing For Script Injection or Cross Site Scripting (XSS)")
-    try:
-        DRIVER.get("https://xenonstack.com/")
-        page_loader()
-
-        printc("[bold yellow][ * ][/bold yellow] Get Started Button Clicked")
-        DRIVER.find_element(By.CLASS_NAME, "nav-button").click()
-        time.sleep(1)
-
-        injkt("<script>alert('Vulnerabile to XSS')</script>")
-
-        printc("[bold yellow][ * ][/bold yellow] Proceed Next Button Clicked")
-        DRIVER.find_element(By.XPATH,
-                            "//p[normalize-space()='Proceed Next']"
-                            ).click()
-
-        if "error" not in DRIVER.page_source:
-            printc("[bug][ x ][/bug] Vulnerability Detected")
-
-        else:
-            printc("[success][ + ][/success]"
-                   " XSS Injection Detection Test Passed")
 
     except Exception:
         exc_type, _, exc_tb = sys.exc_info()
